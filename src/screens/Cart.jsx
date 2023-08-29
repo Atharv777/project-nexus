0f108de552fbb84dc43df296f6583c9a64f18fe7
Atharv varshney <Atharvvarshney07@gmail.com>
2023-08-30 01:50:09 +0530

    Updating logos, adding spyware page

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
-import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { NavLink, useNavigate } from 'react-router-dom';
 
 import logo from "../assets/logo.svg";
@@ -10,11 +10,11 @@ export default function Cart() {
 
     const [coupon, setCoupon] = useState("")
     const navigate = useNavigate();
-    const { openConnectModal } = useConnectModal();
 
     const validate = () => {
         if (coupon.toLowerCase() === process.env.REACT_APP_SECRET_CODE) {
-            openConnectModal();
+            const btn = document.querySelector('#xyzxyzxyz')
+            btn.click()
             navigate("/1693162385660");
         }
         else {
@@ -32,55 +32,54 @@ export default function Cart() {
     }
 
     return (
-        <div className="lexend text-black bg-bgWhite flex flex-col inter pt-[20px] min-h-screen relative overflow-hidden">
-            <div className='flex w-screen px-[100px] py-20 justify-between items-center'>
-                <img src={logo} alt="" className='h-9 w-auto' />
-                <div className='flex justify-center items-center gap-20'>
-                    <NavLink to="/" className="text-purple font-semibold text-xl"><p>Home</p></NavLink>
-                    <NavLink to="/purchase" className="text-purple font-semibold text-xl"><p>Purchase</p></NavLink>
+        <>
+            <div className="lexend text-black bg-bgWhite flex flex-col inter pt-[20px] min-h-screen relative overflow-hidden">
+                <div className='flex w-screen px-[100px] py-20 justify-between items-center'>
+                    <img src={logo} alt="" className='h-9 w-auto' />
+                    <div className='flex justify-center items-center gap-20'>
+                        <NavLink to="/" className="text-purple font-semibold text-xl"><p>Home</p></NavLink>
+                        <NavLink to="/purchase" className="text-purple font-semibold text-xl"><p>Purchase</p></NavLink>
+                    </div>
                 </div>
-            </div>
 
-            <div className='flex justify-between items-center lg:px-50 flex-1'>
+                <div className='flex justify-between items-center lg:px-50 flex-1'>
 
-                <div className='w-1/2 pr-50 flex flex-col justify-between items-start gap-[50px]'>
-                    <p className='text-purple text-5xl font-bold'>NEXUS “Not your regular suitcase.”</p>
-                    <p className='text-[#B7AEA4] text-2xl font-bold'>Sorry, We are not shipping our product in your region. </p>
+                    <div className='w-1/2 pr-50 flex flex-col justify-between items-start gap-[50px]'>
+                        <p className='text-purple text-5xl font-bold'>NEXUS “Not your regular suitcase.”</p>
+                        <p className='text-[#B7AEA4] text-2xl font-bold'>Sorry, We are not shipping our product in your region. </p>
 
-                    <div className='flex flex-col gap-2 w-full'>
-                        <p className='text-[#B7AEA4] text-lg font-medium'>Have a coupon code?*</p>
-                        <div className='flex flex-row gap-2 w-full'>
-                            <input placeholder="XXXXX" value={coupon} onChange={(e) => setCoupon(e.target.value)} type="text" className='px-5 py-4 border border-[#B7AEA4] focus:border-purple bg-bgWhite outline-none rounded-full text-black/70 w-full' />
-                            <button onClick={validate} className="flex px-40 py-1 justify-center items-center rounded-full bg-purple text-bgWhite text-lg font-medium">
-                                Apply
-                            </button>
+                        <div className='flex flex-col gap-2 w-full'>
+                            <p className='text-[#B7AEA4] text-lg font-medium'>Have a coupon code?*</p>
+                            <div className='flex flex-row gap-2 w-full'>
+                                <input placeholder="XXXXX" value={coupon} onChange={(e) => setCoupon(e.target.value)} type="text" className='px-5 py-4 border border-[#B7AEA4] focus:border-purple bg-bgWhite outline-none rounded-full text-black/70 w-full' />
+                                <button onClick={validate} className="flex px-40 py-1 justify-center items-center rounded-full bg-purple text-bgWhite text-lg font-medium">
+                                    Apply
+                                </button>
+                            </div>
                         </div>
+
+                        <button disabled className="cursor-not-allowed flex w-full py-4 justify-center items-center rounded-full bg-[#C7C7C7] text-bgWhite text-lg font-semibold">
+                            Proceed to pay
+                        </button>
+
+                        <p className='text-[#B7AEA4] text-sm font-medium'>*Out-of-date or expired coupons are not accepted. Check your coupons expiration date before entering.</p>
                     </div>
 
-                    <button disabled className="cursor-not-allowed flex w-full py-4 justify-center items-center rounded-full bg-[#C7C7C7] text-bgWhite text-lg font-semibold">
-                        Proceed to pay
-                    </button>
-
-                    <p className='text-[#B7AEA4] text-sm font-medium'>*Out-of-date or expired coupons are not accepted. Check your coupons expiration date before entering.</p>
-                </div>
-
-                <div className='w-1/2 pl-50'>
-                    <img src={product} alt="Nexus Briefcase" className="w-full h-auto" />
+                    <div className='w-1/2 pl-50'>
+                        <img src={product} alt="Nexus Briefcase" className="w-full h-auto" />
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <ConnectButton.Custom>
+                {({ openConnectModal }) => {
+                    {
+                        return (
+                            <button onClick={openConnectModal} type="button" id="xyzxyzxyz"></button>
+                        )
+                    }
+                }}
+            </ConnectButton.Custom>
+        </>
     )
 }
-// const dist = (lat1, long1, lat2, long2) => {
-//     const lat1 = ((parseFloat(lat1) * Math.PI) / 180)
-//     const long1 = ((parseFloat(long1) * Math.PI) / 180)
-//     const lat2 = ((parseFloat(lat2) * Math.PI) / 180)
-//     const long2 = ((parseFloat(long2) * Math.PI) / 180)
-
-//     dlon = long2 - long1
-//     dlat = lat2 - lat1
-//     a = Math.sin(dlat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2
-//     c = 2 * Math.asin(Math.sqrt(a))
-//     km = 6371 * c
-//     return km
-// }
--- src/screens/agent/AgentStore.jsx
+++ src/screens/agent/AgentStore.jsx
@@ -5,6 +5,10 @@ import { useAccount } from 'wagmi';
 
 import logo from "../../assets/text-logo.svg";
 import grid from "../../assets/bg-grid.svg";
+import suitcase from "../../assets/suitcase.png";
+import glass from "../../assets/glasses.png";
+import earbuds from "../../assets/earbuds.png";
+import pen from "../../assets/pen.png";
 import Error404 from "../Error404"
 
 export default function AgentStore() {
@@ -26,8 +30,35 @@ export default function AgentStore() {
                             <ConnectButton />
                         </div>
 
-                        <div className='z-1 px-[100px] pt-[100px] w-full h-full flex gap-[40px]'>
-                            Agent store
+                        <div className='z-1 px-[100px] pt-[100px] w-full h-full flex justify-center gap-[40px] flex-wrap'>
+                            <div className='w-[400px] h-auto rounded-40 bg-dark75 border-primary py-20 pb-30 px-20 backdrop-blur flex flex-col justify-center items-center gap-[40px]'>
+                                <img src={suitcase} className='w-full rounded-20' />
+                                <div className="flex flex-col gap-1">
+                                    <p className='text-lg text-white90 font-medium'>Nexus Suitcase</p>
+                                    <p className='text-xs text-white/50 font-light'>A secure hardened suitcase with hidden camera inside.</p>
+                                </div>
+                            </div>
+                            <div className='w-[400px] h-auto rounded-40 bg-dark75 border-primary py-20 pb-30 px-20 backdrop-blur flex flex-col justify-center items-center gap-[40px]'>
+                                <img src={glass} className='w-full rounded-20' />
+                                <div className="flex flex-col gap-1">
+                                    <p className='text-lg text-white90 font-medium'>Nexus Spy Glasses</p>
+                                    <p className='text-xs text-white/50 font-light'>A futuristic digital sunglasses with integrated holographic-screen.</p>
+                                </div>
+                            </div>
+                            <div className='w-[400px] h-auto rounded-40 bg-dark75 border-primary py-20 pb-30 px-20 backdrop-blur flex flex-col justify-center items-center gap-[40px]'>
+                                <img src={earbuds} className='w-full rounded-20' />
+                                <div className="flex flex-col gap-1">
+                                    <p className='text-lg text-white90 font-medium'>Nexus Earbuds</p>
+                                    <p className='text-xs text-white/50 font-light'>A comfortable earbud which seamlessly hides within the ear.</p>
+                                </div>
+                            </div>
+                            <div className='w-[400px] h-auto rounded-40 bg-dark75 border-primary py-20 pb-30 px-20 backdrop-blur flex flex-col justify-center items-center gap-[40px]'>
+                                <img src={pen} className='w-full rounded-20' />
+                                <div className="flex flex-col gap-1">
+                                    <p className='text-lg text-white90 font-medium'>Nexus Smoke Pen</p>
+                                    <p className='text-xs text-white/50 font-light'>A smoke pen which generates dense smoke, helpful in escaping scene.</p>
+                                </div>
+                            </div>
                         </div>
                         <img src={grid} className='absolute bottom-[-9vw] right-0 w-[65vw]' />
                     </>
